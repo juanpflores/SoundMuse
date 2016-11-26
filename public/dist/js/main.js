1713dbb17f005f7608e9c4e2af65afa6f787e62e
Juan Pablo <juanpflores94@gmail.com>
2016-11-26 05:42:49 -0600

    finished structure of dashboard

---
+++ public/dist/js/main.js
@@ -0,0 +1,16 @@
+// Sets up shortcuts to Firebase features and initiate firebase auth.
+hack.prototype.initFirebase = function() {
+  // Shortcuts to Firebase SDK features.
+  this.auth = firebase.auth();
+  this.database = firebase.database();
+  this.storage = firebase.storage();
+  // Initiates Firebase auth and listen to auth state changes.
+  this.auth.onAuthStateChanged(this.onAuthStateChanged.bind(this));
+};
+
+// Signs-in Friendly Chat.
+hack.prototype.signIn = function() {
+  // Sign in Firebase using popup auth and Google as the identity provider.
+  var provider = new firebase.auth.GoogleAuthProvider();
+  this.auth.signInWithPopup(provider);
+};
